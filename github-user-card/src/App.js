06bb2717de6c00d11a725ee9c1556a1d46a35e55
Junior Dugue <jrdugue@gmail.com>
2019-10-05 13:25:37 -0400

    installed axios, was able to log out api data via cdm in console, added default state place holders

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      login: "",
+      name: "",
+      company: "",
+      location: "",
+      bio: "",
+      followers: "",
+      following: "",
+    }
+  }
+
+  handleChange = e => {
+    this.setState({
+      name: e.target.value
+    }) 
+  }
+
+  componentDidMount(){
+    axios.get("https://api.github.com/users/juniordugue")
+    // axios.get(`https://api.github.com/users/${this.state.name}`)
+    .then(res => {
+      console.log(res)
+    })
+    .catch(err => {
+      console.log('error', err)
+    })
+  }
+
+    render(){
+      return (
+        <div className="App">
+          <h1>Github User Card</h1>
+
+        </div>
+    );
+  }
 }
 
 export default App;
